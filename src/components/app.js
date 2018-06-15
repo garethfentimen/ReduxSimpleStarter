6f484fe10a523e1e9876ea7c3d9aacbae3410f90
Gareth J. Fentimen <gareth.j.fentimen@avanade.com>
2018-06-15 16:09:08 -0400

    React app for Section 2

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import YtSearch from 'youtube-api-search';
+import VideoList from './video-list';
+import _ from 'lodash';
+import VideoDetail from './video-detail';
+import SearchBar from './search-bar';
+const apiKey  = 'AIzaSyB-n0sx1CmL--DEXBT340GdIqLDwc-Om7w';
+
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      videos: [], 
+      selectedVideo: null 
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YtSearch({ key: apiKey, term}, (videos) => {
+      this.setState({ videos, selectedVideo: videos[0] });
+    });
+  }
+
   render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+          videos={this.state.videos} />
+      </div>
     );
   }
 }
